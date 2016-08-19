aa7efe60684a6e27c5e67f4978ec0dadd327f279
Jon Balon <development@jonbalon.com>
2016-08-19 10:44:45 -0400

    Added functionality to get IP, language, and OS

--- server.js
+++ server.js
@@ -2,11 +2,54 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world');
+app.get('/identify', function(req, res) {
+
+  // Extracts operating system information from the browser headers.
+  var extractOperatingSystem = function(userAgent) {
+
+    // Rule to find text between parentheses.
+    var regex = /\(([^)]+)\)/;
+    var match = regex.exec(userAgent);
+
+    // Return the text in the first parenthesis.
+    return match[1];
+
+  }
+
+  // Extracts language from the browser headers.
+  var extractLanguage = function(data) {
+
+    // Split the given header by commas.
+    var tokens = data.split(',');
+
+    // First token is the language.
+    return tokens[0];
+
+  }
+
+  // Store results in an object.
+  var results = {
+    'ipaddress': '',
+    'language': '',
+    'software': ''
+  };
+
+  // Get browser headers.
+  var headers = req.headers;
+
+  // Store ip address.
+  results['ipaddress'] = req.ip;
+
+  // Store language.
+  results['language'] = extractLanguage(headers['accept-language']);
+
+  // Store operating system.
+  results['software'] = extractOperatingSystem(headers['user-agent']);
+
+  res.json(results);
 });
 
 // Listen on Port 8080.
 app.listen(8080, function() {
-  console.log('Listening for incoming traffic on PORT 8080');
-})
+  console.log('Listening for incoming traffic on PORT 8080.');
+});
